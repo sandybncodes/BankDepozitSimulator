86d2aefa47f67e2b24731586f2cfa3ef19e235c8
sandubncodes <sandubn2002@gmail.com>
2024-03-02 22:26:19 +0200

    updated login code

--- BankDepozitSimulator/MainWindow.xaml.cs
+++ BankDepozitSimulator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,13 +22,12 @@ namespace BankDepozitSimulator
     public partial class MainWindow : Window
     {
         Login login = new Login();
-        DatabaseConnection connection = new DatabaseConnection();
+        DatabaseConnection cnn = new DatabaseConnection();
         public MainWindow()
         {
             InitializeComponent();
-            connection.connect();
-            
-            //Login login = new Login();
+            cnn.connect();
+
             login.loginButton.Click += LoginButtonClick;
             main_grid.Children.Add(login);
         }
@@ -35,21 +35,63 @@ namespace BankDepozitSimulator
         private void LoginButtonClick(object sender, RoutedEventArgs e)
         {
             User user = new User();
-            
+
             string username = login.usernameTextBox.Text;
             string password = login.passwordTextBox.Password;
 
-            if(username == "test" && password == "1234")
+            if (username != "" && password != "")
             {
-                main_grid.Children.Clear();
+                try
+                {
+                    string dbSSN = "";
+                    string dbUsername = "";
+                    string dbPassword = "";
+                    int dbAccountID = 0;
+                    int dbBankAccountID = 0;
+                    int dbBankID = 0;
+                    //string sqlQuery = $"SELECT ssn, username, password FROM USERS.ACCOUNTS WHERE username = '{username}'";
+                    string sqlQuery = $"EXEC selectLoginCredentials @username = '{username}'";
+                    SqlCommand sqlCommand = new SqlCommand(sqlQuery, cnn.connection);
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
 
-                Dashboard dashboard = new Dashboard();
-                dashboard.userLabel.Content = username + " " + password;
-                main_grid.Children.Add(dashboard);
+                    while (sqlDataReader.Read())
+                    {
+                        dbSSN = sqlDataReader["ssn"].ToString();
+                        dbUsername = sqlDataReader["username"].ToString();
+                        dbPassword = sqlDataReader["password"].ToString();
+                        dbAccountID = int.Parse(sqlDataReader["account_id"].ToString());
+                        dbBankAccountID = int.Parse(sqlDataReader["bank_account_id"].ToString());
+                        dbBankID = int.Parse(sqlDataReader["bank_id"].ToString());
+                    }
+
+                    if (dbUsername == username && dbPassword == password)
+                    {
+                        //user.setUserSSN(dbSSN);
+                        User.userSSN = dbSSN;
+                        User.username = dbUsername;
+                        User.password = dbPassword;
+                        User.accountID = dbAccountID;
+                        User.bankAccountID = dbBankAccountID;
+                        User.bankID = dbBankID;
+                        main_grid.Children.Clear();
+
+                        Dashboard dashboard = new Dashboard();
+                        dashboard.userLabel.Content = User.username;
+                        main_grid.Children.Add(dashboard);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
             else
             {
-                MessageBox.Show("Incorrect username or password!", "Login error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                MessageBox.Show("Username and password should not be empty!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
     }
